8f0fd104e33214c4207912c6e76611dc0567160e
Leo <leonardo.ayalao@pucp.edu.pe>
2021-11-23 20:21:58 -0500

    Servlets

---
+++ src/main/java/pucp/edu/pe/labcito9/PaisesServlet.java
@@ -0,0 +1,19 @@
+package pucp.edu.pe.labcito9;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "PaisesServlet", value = "/PaisesServlet")
+public class PaisesServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
